e01e3c3d5597af8a0ac6dd27862a043c934ff0a7
tiimurr <tasecov01@educantabria.es>
2025-03-10 15:39:55 +0100

    Hola

--- src/main/java/torneofutbol/MainTorneoFutbol.java
+++ src/main/java/torneofutbol/MainTorneoFutbol.java
@@ -5,7 +5,7 @@ public class MainTorneoFutbol {
 
     public static void main(String[] args) {
 
-        System.out.println("Accediendo a la base de datos del torneo de FP");
+        System.out.println("Accediendo a los jugadores del torneo");
         
         // Crear una instancia de la clase Equipo
         Equipo equipoMH = new Equipo("IES MH FC");
